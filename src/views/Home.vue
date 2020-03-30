1277a9a68e42bee41fe1152ef56dbb593f6f4a63
omohemma <emmaomoh@gmail.com>
2020-03-30 12:26:32 +0100

    Iterate cards with dynamic data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -62,86 +62,37 @@
       <!-- Card Box -->
       <div class="flex flex-wrap mx-4">
         <!-- Cards Begin-->
-        <div class="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3">
+        <div
+          class="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3"
+          v-for="destination in popularDestinations"
+          :key="destination"
+        >
           <div
             class="flex items-center rounded-lg bg-white shadow-lg overflow-hidden"
           >
             <img
               class="h-32 w-32 flex-shrink-0"
-              src="/images/toronto.jpg"
-              alt="Toronto skyline"
+              :src="destination.imageUrl"
+              :alt="destination.imageAlt"
             />
             <div class="px-6 py-4">
               <h3 class="text-lg font-semibold text-gray-800">
-                Toronto
+                {{ destination.city }}
               </h3>
               <p class="text-gray-600">
-                120
+                ${{ destination.averagePrice }}/night average
               </p>
               <div class="mt-4">
                 <a
                   href="#"
                   class="text-indigo-500 hover:text-indigo-400 font-semibold text-sm"
-                  >Explore 76 properties</a
+                  >Explore {{ destination.propertyCount }} properties</a
                 >
               </div>
             </div>
           </div>
         </div>
 
-        <div class="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3">
-          <div
-            class="flex items-center rounded-lg bg-white shadow-lg overflow-hidden"
-          >
-            <img
-              class="h-32 w-32 flex-shrink-0"
-              src="/images/toronto.jpg"
-              alt="Toronto skyline"
-            />
-            <div class="px-6 py-4">
-              <h3 class="text-lg font-semibold text-gray-800">
-                Toronto
-              </h3>
-              <p class="text-gray-600">
-                120
-              </p>
-              <div class="mt-4">
-                <a
-                  href="#"
-                  class="text-indigo-500 hover:text-indigo-400 font-semibold text-sm"
-                  >Explore 76 properties</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3">
-          <div
-            class="flex items-center rounded-lg bg-white shadow-lg overflow-hidden"
-          >
-            <img
-              class="h-32 w-32 flex-shrink-0"
-              src="/images/toronto.jpg"
-              alt="Toronto skyline"
-            />
-            <div class="px-6 py-4">
-              <h3 class="text-lg font-semibold text-gray-800">
-                Toronto
-              </h3>
-              <p class="text-gray-600">
-                120
-              </p>
-              <div class="mt-4">
-                <a
-                  href="#"
-                  class="text-indigo-500 hover:text-indigo-400 font-semibold text-sm"
-                  >Explore 76 properties</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
         <!-- Cards End -->
       </div>
     </div>

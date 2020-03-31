023bd55058c49cdabac700cb843a26f4885d175f
omohemma <emmaomoh@gmail.com>
2020-03-31 11:20:46 +0100

    10. Designing with Tailwind CSS Making Text Content Feel Designed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="antialiased text-gray-900">
     <router-view />
   </div>
 </template>
--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -3,15 +3,20 @@
     <img :src="property.imageUrl" alt="property.imageAlt" />
 
     <div class="p-6">
-      <h4>{{ property.title }}</h4>
-      <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
-      <div>
-        {{ property.formattedPrice }} /
-        <i class="fa fa-wikipedia-w" aria-hidden="true"></i>
+      <div
+        class="text-gray-600 text-xs uppercase font-semibold tracking-wide"
+      >{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
+
+      <h4 class="font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
+
+      <div class=" mt-1">
+        {{ property.formattedPrice }}
+        <span class="text-gray-600 text-sm">/ wk</span>
       </div>
-      <div>
-        {{ property.rating }}/ 5 Stars (based on
-        {{ property.reviewCount }} reviews)
+      <div class="mt-4">
+        <span class="text-teal-600 font-semibold">{{ property.rating }}/ 5 Stars</span>
+
+        <span class="text-gray-600 text-sm"> (based on {{ property.reviewCount }} reviews)</span>
       </div>
     </div>
   </div>

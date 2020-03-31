5a215a6e0cba1125f82cd055c7bc1fc0e0add812
omohemma <emmaomoh@gmail.com>
2020-03-31 10:36:12 +0100

    Designing with Tailwind CSS Structuring a Basic Card

---
+++ src/components/PropertyCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-white rounded-lg overflow-hidden border shadow-lg">
+    <img :src="property.imageUrl" alt="property.imageAlt" />
+
+    <div class="p-6">
+      <h4>{{ property.title }}</h4>
+      <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
+      <div>
+        {{ property.formattedPrice }} /
+        <i class="fa fa-wikipedia-w" aria-hidden="true"></i>
+      </div>
+      <div>
+        {{ property.rating }}/ 5 Stars (based on
+        {{ property.reviewCount }} reviews)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Property Card",
+  props: {
+    property: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Card.vue
+++ src/views/Card.vue
@@ -1,11 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <div class="bg-gray-200 min-h-full p-8 flex items-center justify-center">
+      <PropertyCard :property="property" />
+    </div>
+  </div>
 </template>
 
 <script>
+import PropertyCard from "@/components/PropertyCard.vue";
 export default {
-  name: "Navigation"
+  name: "Navigation",
+  components: {
+    PropertyCard
+  },
+  data() {
+    return {
+      property: {
+        imageUrl:
+          "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+        imageAlt: "Rear view of modern home with pool",
+        beds: 3,
+        baths: 2,
+        title: "Modern home in city center",
+        priceInCents: 190000,
+        formattedPrice: "$1,900.00",
+        reviewCount: 34,
+        rating: 4
+      }
+    };
+  }
 };
 </script>
-
-<style lang="scss" scoped></style>

59c4dff8d86e15b052d4359225d3a64b3c0652ff
omohemma <emmaomoh@gmail.com>
2020-04-02 15:24:49 +0100

    Designing with Tailwind CSS Creating Depth with Shadows and Layers

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white rounded-lg overflow-hidden border shadow-lg">
+  <div>
     <!-- overflow-hidden make images wrap with div container -->
 
     <!-- IE 11 Hack -->
@@ -17,49 +17,51 @@
     
     -->
 
-    <div class="bg-red-500 relative pb-2/3">
+    <div class="bg-red-500 relative pb-5/6">
       <img
-        class="absolute h-full w-full object-cover"
+        class="absolute h-full w-full object-cover rounded-lg shadow-md"
         :src="property.imageUrl"
         :alt="property.imageAlt"
       />
     </div>
 
-    <div class="p-6">
-      <div class="flex items-baseline">
-        <!-- 
+    <div class="relative px-4 -mt-12">
+      <div class="bg-white p-6 rounded-lg shadow-lg">
+        <div class="flex items-baseline">
+          <!-- 
           badge could be -> bg-teal-500 text-white 
           yet it competes with the main focus of the 
           card which is Property Title
-        -->
-        <span
-          class="inline-block bg-teal-200 text-teal-800 px-2 rounded-full text-xs uppercase font-semibold tracking-wide"
-        >New</span>
-        <div
-          class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide"
-        >{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
-      </div>
+          -->
+          <span
+            class="inline-block bg-teal-200 text-teal-800 px-2 rounded-full text-xs uppercase font-semibold tracking-wide"
+          >New</span>
+          <div
+            class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide"
+          >{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
+        </div>
 
-      <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
+        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ property.title }}</h4>
 
-      <div class="mt-1">
-        {{ property.formattedPrice }}
-        <span class="text-gray-600 text-sm">/ wk</span>
-      </div>
-      <div class="mt-2 flex items-center">
-        <!-- SVG -->
-        <svg
-          v-for="i in 5"
-          :key="i"
-          class="h-4 w-4 fill-current"
-          :class="i <= property.rating ? 'text-teal-500' : 'text-gray-300'"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
-          />
-        </svg>
-        <span class="text-gray-600 text-sm ml-2">{{ property.reviewCount }} reviews</span>
+        <div class="mt-1">
+          {{ property.formattedPrice }}
+          <span class="text-gray-600 text-sm">/ wk</span>
+        </div>
+        <div class="mt-2 flex items-center">
+          <!-- SVG -->
+          <svg
+            v-for="i in 5"
+            :key="i"
+            class="h-4 w-4 fill-current"
+            :class="i <= property.rating ? 'text-teal-500' : 'text-gray-300'"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+            />
+          </svg>
+          <span class="text-gray-600 text-sm ml-2">{{ property.reviewCount }} reviews</span>
+        </div>
       </div>
     </div>
   </div>

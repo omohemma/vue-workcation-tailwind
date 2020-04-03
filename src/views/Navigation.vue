580edf38f82b73aa1156cc39567b3f671d92825c
omohemma <emmaomoh@gmail.com>
2020-04-03 21:02:38 +0100

    Designing with Tailwind CSS Building a Navbar Layout with Flexbox

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <header class="bg-gray-900 flex items-end justify-between py-3 px-4">
+    <div>
+      <img class="h-8" src="../../public/images/logo.svg" alt />
+    </div>
+
+    <div>
+      <button
+        type="button"
+        class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+      >
+        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+          <path
+            fill-rule="evenodd"
+            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+          />
+        </svg>
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>

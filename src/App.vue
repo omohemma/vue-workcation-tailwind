507fc68dd4144e044080d36bd0d006519937c21f
omohemma <emmaomoh@gmail.com>
2020-04-03 22:12:30 +0100

    Designing with Tailwind CSS Toggling the Navbar Links on Mobile

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div id="app" class="antialiased text-gray-900">
-    <router-view />
+  <div>
+    <Navigation />
+    <div id="app" class="antialiased text-gray-900">
+      <router-view />
+    </div>
   </div>
 </template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+export default {
+  name: "app",
+  components: {
+    Navigation
+  }
+};
+</script>

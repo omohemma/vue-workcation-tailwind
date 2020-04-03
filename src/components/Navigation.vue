4d828d0e531f74d244104aaef28bc562f72b22f4
omohemma <emmaomoh@gmail.com>
2020-04-03 22:56:15 +0100

    Designing with Tailwind CSS Making the Navbar Responsive

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,13 @@
 <template>
-  <header class="bg-gray-900">
-    <div class="flex items-end justify-between py-3 px-4">
+  <header class="bg-gray-900 sm:flex sm:justify-between items-center sm:py-3 sm:px-4">
+    <div class="flex items-end justify-between py-3 px-4 sm:p-0">
       <div>
         <img class="h-8" src="../../public/images/logo-inverted.svg" alt />
       </div>
       <div>
         <button
           type="button"
-          class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+          class="block text-gray-500 hover:text-white focus:text-white focus:outline-none sm:hidden"
           @click="isActive"
         >
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
@@ -25,18 +25,21 @@
         </button>
       </div>
     </div>
-    <div class="px-2 pt-2 pb-4" :class="isOpened === true ? 'block' : 'hidden'">
+    <div
+      class="px-2 pt-2 pb-4 sm:block sm:flex sm:p-0"
+      :class="isOpened === true ? 'block' : 'hidden'"
+    >
       <a
         href="#"
-        class="mt-1 block text-white font-semibold py-1 px-2 rounded hover:bg-gray-800"
+        class="block text-white font-semibold py-1 px-2 rounded hover:bg-gray-800"
       >List your property</a>
       <a
         href="#"
-        class="mt-1 block text-white font-semibold py-1 px-2 rounded hover:bg-gray-800"
+        class="mt-1 block text-white font-semibold py-1 px-2 rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
       >Trips</a>
       <a
         href="#"
-        class="mt-1 block text-white font-semibold py-1 px-2 rounded hover:bg-gray-800"
+        class="mt-1 block text-white font-semibold py-1 px-2 rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
       >Messages</a>
     </div>
   </header>
